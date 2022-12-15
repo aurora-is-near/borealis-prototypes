d789455913decf8375d2003d1486b18a12cc8add
Roman Hodulák <roman.hodulak@aurora.dev>
2022-12-15 23:39:06 +0100

    refactor: Split `message.proto` into multiple files.

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["proto/message.proto"], &["proto/"])?;
+    prost_build::compile_protos(&["proto/message.proto"], &[
+        "proto/",
+        "proto/payloads",
+        "proto/payloads/near_block",
+        "proto/payloads/near_block/transaction",
+    ])?;
     Ok(())
 }
